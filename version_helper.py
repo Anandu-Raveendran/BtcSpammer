7197847a1e5bcd583ebe8d2d962a8d935bf3a759
Anandu <47anandu007@gmail.com>
2022-09-28 23:57:19 -0400

    Update version_helper.py

--- version_helper.py
+++ version_helper.py
@@ -1,12 +1,30 @@
 import os
 from datetime import datetime
 
+
+"""
+Usage: python-c "import version_helper; version_helper.run_in_all_dir()
+
+Call this from the parent dir.
+It traverses the immediate sub dirs and executes get_local_repo_version()
+
+"""
 def run_in_all_dir():
     
     for path in os.listdir("."):
         print(path)
         if os.path.isdir(path):
             get_local_repo_version("",path)
+
+
+"""
+Usage: python-c "import version_helper; version_helper.get_local_repo_version()"
+
+Call this in the dir to get the git changes.
+it saves to a new file named YYYY_MM_DD_HH_MM_test_software_version.txt
+
+Throws exception in case of error.
+"""
 
 def get_local_repo_version(fname = "", path = "."):    
 
@@ -41,6 +59,16 @@ def get_local_repo_version(fname = "", path = "."):
     cmd = "echo rc: "+ str(rc) +" >> "+out_file_name
     os.system(cmd)
 
+
+"""
+Usage: python-c "import version_helper; version_helper.get_local_repo_version_with_file(fileName)"
+
+Call this in the dir to get the git changes.
+It saves data to the fileName file provided as argument
+
+Throws exception in case of error.
+"""
+
 def get_local_repo_version_with_file(fname = ''):
 
     if fname:
@@ -48,3 +76,4 @@ def get_local_repo_version_with_file(fname = ''):
     else:
         print("Usage: get_local_repo_version_with_file('file_name')\n")
         raise Exception("Need filename as argument to save to.")
+
