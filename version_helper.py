2ab39b81b52776508c0f72ec40ba2ba66746ff93
Anandu <47anandu007@gmail.com>
2022-10-03 21:36:07 -0400

    Update version_helper.py

--- version_helper.py
+++ version_helper.py
@@ -11,58 +11,60 @@ Call this in the curr dir to get the git changes.
 
 Throws exception in case of error.
 """
+class GitVersionChecker():
+   
+    def __run_cmd(self, cmd:str):
+        with Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) as proc:
+            proc.wait(timeout=60)
+            std_output = proc.stdout.read().decode()
+            std_error = proc.stderr.read().decode()
+            rc = proc.returncode
 
-def get_local_repo_version():      
-
-    ret1 = run_cmd("git reflog -1")
-    ret2 = run_cmd("git status")
-
-    ret =  [ ret1, ret2 ]
-    return ret
-
-def run_cmd(cmd:str):
-    with Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) as proc:
-        proc.wait(timeout=60)
-        std_output = proc.stdout.read().decode()
-        std_error = proc.stderr.read().decode()
-        rc = proc.returncode
-
-        if(rc != 0):
-            errorstr = "git status error: " + str(rc)
-            raise Exception(errorstr)
-        return (std_output, std_error, rc)
+            if(rc != 0):
+                errorstr = "git status error: " + str(rc)
+                raise Exception(errorstr)
+            return (std_output, std_error, rc)
     
 
+    def get_local_repo_version(self):      
 
-"""
-Usage: python-c "import version_helper; version_helper.get_local_repo_version_with_file(<file_name>, <repo_path>)"
-NOTE: both the arguments are default and not compulsory.
+        ret1 = self.__run_cmd("git reflog -1")
+        ret2 = self.__run_cmd("git status")
 
-It saves the tuple from get_local_repo_version to 
-a new file named YYYY_MM_DD_HH_MM_test_software_version.txt or
-to the file name file provided as argument
+        ret =  [ ret1, ret2 ]
+        return ret
 
-Throws exception in case of error.
-"""
+   
 
-def get_local_repo_version_with_file(fname:str = "", repo_path:str = ""):
+    """
+    Usage: python-c "import version_helper; version_helper.get_local_repo_version_with_file(<file_name>, <repo_path>)"
+    NOTE: both the arguments are default and not compulsory.
+
+    It saves the tuple from get_local_repo_version to 
+    a new file named YYYY_MM_DD_HH_MM_test_software_version.txt or
+    to the file name file provided as argument
+
+    Throws exception in case of error.
+    """
+
+    def get_local_repo_version_with_file(self, fname:str = "", repo_path:str = ""):
     
-    if(repo_path == ""):
-        repo_path = os.path.abspath(os.path.join(__file__ ,"../.."))
+        if(repo_path == ""):
+            repo_path = os.path.abspath(os.path.join(__file__ ,"../.."))
     
-    if(repo_path != "."):
-        os.chdir(repo_path)
+        if(repo_path != "."):
+            os.chdir(repo_path)
 
-    if fname:
-        out_file_name = fname
-    else:
-        dateTimeObj = datetime.now()
-        out_file_name = dateTimeObj.strftime("%Y_%m_%d_%H_%M")
-        out_file_name += "_test_software_version.txt"
+        if fname:
+            out_file_name = fname
+        else:
+            dateTimeObj = datetime.now()
+            out_file_name = dateTimeObj.strftime("%Y_%m_%d_%H_%M")
+            out_file_name += "_test_software_version.txt"
 
-    ret_list = get_local_repo_version()
+        ret_list = self.get_local_repo_version()
 
-    cmd = "STD_OUT: " + str(ret_list[0][0]) + "\n"      \
+        cmd = "STD_OUT: " + str(ret_list[0][0]) + "\n"      \
             + str(ret_list[1][0]) + "\n"                \
                                                         \
             +"STD_ERR: " + str(ret_list[0][1]) + "\n"   \
@@ -71,7 +73,7 @@ def get_local_repo_version_with_file(fname:str = "", repo_path:str = ""):
             +"RC: " + str(ret_list[0][2]) + "\n"        \
             + str(ret_list[1][2]) + "\n"
 
-    with open(out_file_name, "a") as f:
-        f.write(cmd)
-        f.close()
+        with open(out_file_name, "a") as f:
+            f.write(cmd)
+            f.close()
     
