f12532d21de9e0eff04118f6849d6bcf8afa58b7
Anandu <47anandu007@gmail.com>
2022-10-06 21:20:51 -0400

    Update version_helper.py

--- version_helper.py
+++ version_helper.py
@@ -4,15 +4,18 @@ from subprocess import Popen
 from datetime import datetime
 from subprocess import Popen, PIPE, STDOUT
 
-"""
-Usage: python-c "import version_helper; version_helper.get_local_repo_version()"
-
-Call this in the curr dir to get the git changes.
-
-Throws exception in case of error.
-"""
 class GitVersionChecker():
+
+    """
+    This is a helper function which executed commands passed to it.
+
+    Args: 
+        cmd (str): The command to be executed on the shell.  
+    Returns:
+        typle( std_out(str), std_err(str), rc(int) ): returns a tuple of std out, std err and return code(rc)  
    
+    """
+
     def __run_cmd(self, cmd:str) -> tuple[str, str, int]:
         with Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) as proc:
             proc.wait(timeout=60)
@@ -22,6 +25,14 @@ class GitVersionChecker():
 
             return (std_output, std_error, rc)
     
+    """
+    Gets the git rev information, modification check and release check.
+
+    Returns:
+        list[tuple[str, str, int], tuple[str, str, int], tuple[str, str, int]]:      
+        Returns a list of tuples in the order: git reflog output, git status output and git describe output respectively
+    
+    """
 
     def get_local_repo_version(self) -> list[tuple[str, str, int], tuple[str, str, int], tuple[str, str, int]]:      
 
@@ -35,14 +46,14 @@ class GitVersionChecker():
    
 
     """
-    Usage: python-c "import version_helper; version_helper.get_local_repo_version_with_file(<file_name>, <repo_path>)"
-    NOTE: both the arguments are default and not compulsory.
-
-    It saves the tuple from get_local_repo_version to 
+    It saves the tuples returned from get_local_repo_version() to 
     a new file named YYYY_MM_DD_HH_MM_test_software_version.txt or
-    to the file name file provided as argument
+    to the file name file provided as function argument
 
-    Throws exception in case of error.
+    args:
+        fname(str): the file path to save to.
+        repo_path(str): the path where the git commands should execute. Usually root of the project.
+        
     """
 
     def get_local_repo_version_with_file(self, fname:str = "", repo_path:str = "") -> None:
