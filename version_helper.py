785d29e9437d9f61532e123f0469d4ba5dc6f72d
Anandu <47anandu007@gmail.com>
2022-10-10 22:24:37 -0400

    Update version_helper.py

--- version_helper.py
+++ version_helper.py
@@ -5,18 +5,18 @@ from subprocess import Popen, PIPE
 
 
 class GitVersionChecker():
-
-    """
-    This is a helper function which executed commands passed to it.
-
-    Args:
-        cmd (str): The command to be executed on the shell.
-    Returns:
-        typle( std_out(str), std_err(str), rc(int) ):
-                returns a tuple of std out, std err and return code(rc)
-    """
-
     def __run_cmd(self, cmd: str) -> Tuple[str, str, int]:
+        """
+        This is a helper function which executed commands passed to it.
+        This function is called only by the other functions in the file.
+
+        Args:
+            cmd (str): The command to be executed on the shell.
+        Returns:
+            typle( std_out(str), std_err(str), rc(int) ):
+                returns a tuple of std out, std err and return code(rc)
+        """
+
         with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True) as proc:
             proc.wait(timeout=60)
             std_output = proc.stdout.read().decode()
@@ -25,16 +25,19 @@ class GitVersionChecker():
 
             return (std_output, std_error, rc)
 
-    """
-    Gets the git rev information, modification check and release check.
-
-    Returns:
-        list[tuple[str, str, int], tuple[str, str, int], tuple[str, str, int]]:
-        Returns a list of tuples in the order:
-        git reflog output, git status output and git describe output.
-    """
-
     def get_local_repo_version(self) -> List[Tuple[str, str, int]]:
+        """
+        Gets the git rev information, modification check and release check.
+        It executes the commands git reflog -1, git status and git describe
+        and returns the result as a list of tuples.
+
+        Returns:
+            list[tuple[str, str, int]]:
+            Returns the results of the commands:-
+            git reflog at list[0],
+            git status at list[1] and
+            git describe at list[2].
+        """
 
         ret1 = self.__run_cmd("git reflog -1")
         ret2 = self.__run_cmd("git status")
@@ -43,23 +46,24 @@ class GitVersionChecker():
         ret = [ret1, ret2, ret3]
         return ret
 
-    """
-    It saves the tuples returned from get_local_repo_version() to
-    a new file named YYYY_MM_DD_HH_MM_test_software_version.txt or
-    to the file name file provided as function argument
-
-    args:
-        fname(str): the file path to save to.
-        repo_path(str): the path where the git commands should execute.
-        Usually repo_path should be root of the git project.
-
-    """
-
     def get_local_repo_version_with_file(
         self,
         fname: str = "",
         repo_path: str = ""
     ) -> None:
+
+        """
+        It saves the tuples returned from get_local_repo_version() to
+        a new default file named YYYY_MM_DD_HH_MM_test_software_version.txt or
+        to the file name file provided as a function argument(repo_path)
+
+        args:
+            fname(str): the file path to save to instead of default file.
+            repo_path(str): the path where the git commands should execute.
+            By default it executes two folder above the current dir.
+            Usually repo_path should be root of the git project.
+
+        """
 
         if (repo_path == ""):
             repo_path = os.path.abspath(os.path.join(__file__, "../../.."))
@@ -95,5 +99,5 @@ class GitVersionChecker():
             f.close()
 
 
-g1 = GitVersionChecker()
-g1.get_local_repo_version_with_file()
+gvc = GitVersionChecker()
+gvc.get_local_repo_version_with_file()
